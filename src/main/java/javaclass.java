1b1973e02938989e40920aaf25b06545dad2a71d
ganesh <ganeshbachategmb999@gmail.com>
2021-01-18 10:13:46 +0530

    18-01-21 first commit

--- src/main/java/javaclass.java
+++ src/main/java/javaclass.java
@@ -1,5 +1,5 @@
 public class javaclass {
     public static void main(String[] args) {
-        System.out.println("hello enjoy monday");
+        System.out.println("hello enjoy monday 18-01-21");
     }
 }
